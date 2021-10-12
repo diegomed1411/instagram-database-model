991625749028b0f260585eae89fdc28bdc8eaaaf
Diego Medina <cr.diegomedina@gmail.com>
2021-10-12 21:05:54 +0000

    ejercicio completado

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+import enum
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -15,30 +16,65 @@ class User(Base):
     firstname = Column(String(100), nullable=False)
     lastname = Column(String(100), nullable=False)
     email = Column(String(100), nullable=False)
+    follower = relationship('Followers', backref='followers')
+    post = relationship('Post', backref='post')
+    comment = relationship('Comment', backref='comment')
 
 class Followers(Base):
     __tablename__ = 'followers'
     id = Column(Integer, primary_key=True)
     user_from_id = Column(Integer, ForeignKey('user.id'))
     user_to_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Enumerado (enum.Enum):
+     foto = 1
+     video = 2
+     reels = 3
+     igtv = 4
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Media (Base):
+    __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    type = Column(Enum(Enumerado))
+    url = Column(String(100))
+    post_id = Column(Integer, ForeignKey('post.id'))
+#    post = relationship(Post)
+
+class Post (Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    comment = relationship('Comment', backref='comment')
+    media = relationship('Media', backref='media')
+
+class Comment (Base):
+    __tablename__ = "comment"
+    id = Column(Integer, primary_key=True)    
+    comment_text = Column(String(250))
+    author_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+    
+# class Person(Base):
+#    __tablename__ = 'person'
+#    # Here we define columns for the table person
+#    # Notice that each column is also a normal Python instance attribute.
+#    id = Column(Integer, primary_key=True)
+#    name = Column(String(250), nullable=False)
+
+#class Address(Base):
+#    __tablename__ = 'address'
+#    # Here we define columns for the table address.
+#    # Notice that each column is also a normal Python instance attribute.
+#    id = Column(Integer, primary_key=True)
+#    street_name = Column(String(250))
+#    street_number = Column(String(250))
+#    post_code = Column(String(250), nullable=False)
+#    person_id = Column(Integer, ForeignKey('person.id'))
+#    person = relationship(Person)
 
     def to_dict(self):
         return {}
